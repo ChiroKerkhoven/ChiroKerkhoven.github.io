a3e3af18bb3d5a265e8d6f7bf5ad12bc893e2228
Dieter Claessens <45936325+DieterClaessens67@users.noreply.github.com>
2022-09-19 23:48:46 +0200

    made nav look normal again

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
-import { EvenementenComponent } from './components/evenementen/evenementen.component';
-import { CalenderComponent } from './components/calender/calender.component';
-import { OnzeGroepenComponent } from './components/onze-groepen/onze-groepen.component';
-import { ContactComponent } from './components/contact/contact.component';
 
 
 const routes: Routes = [
-  {path: 'home', component: HomeComponent},
-  {path: 'evenementen', component: EvenementenComponent},
-  {path: 'kalender', component: CalenderComponent},
-  {path: 'onze-groepen', component: OnzeGroepenComponent},
-  {path: 'contact', component: ContactComponent},
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: '**', redirectTo: 'home', pathMatch: 'full' }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { EvenementenComponent } from './components/evenementen/evenementen.component';
-import { EvenementComponent } from './components/evenement/evenement.component';
-import { CalenderComponent } from './components/calender/calender.component';
-import { OnzeGroepenComponent } from './components/onze-groepen/onze-groepen.component';
-import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    NavbarComponent,
-    EvenementenComponent,
-    EvenementComponent,
-    CalenderComponent,
-    OnzeGroepenComponent,
-    ContactComponent
   ],
   imports: [
     BrowserModule,
